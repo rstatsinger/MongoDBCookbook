a93550d649bc0b234df18698c15e2adc99c42034
rstatsinger <rstatsinger@gmail.com>
2017-11-11 12:49:23 -0800

    Initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,69 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+user 'mongo' do
+	home '/opt/mongodb'
+end
+
+group 'mongo' do
+	members 'mongo'
+end
+
+directory '/opt/mongodb' do
+	group 'mongo'
+  	mode '0775'
+ 	not_if do
+ 		File.exist?('/opt/mongodb')
+ 	end
+end
+
+# package 'mongo-db-3.4'
+
+# template for the mongo yum repo
+
+template '/etc/yum.repos.d/mongodb-org-3.4.repo' do
+	source 'mongoYumRepo.erb'
+	owner 'mongo'
+#   	notifies :restart, 'service[mongod ]',:immediate
+end
+
+# template for mongod.conf - future
+
+# template '/etc/mongod.conf' do
+# 	source 'mongoConf.erb'
+# 	owner 'mongo'
+#   	notifies :restart, 'service[mongod ]',:immediate
+# end
+
+# execute sudo yum install -y mongodb-org
+
+execute 'sudo yum install -y mongodb-org' do
+end
+
+
+execute 'chgrp -R mongo /opt/mondogb' do
+ 	only_if { Etc.getgrgid(File.stat('/opt/mongodb').gid).name != 'mongo' }
+end
+
+execute 'chmod ug+rwx /opt/mongodb' do
+  	# only_if { (File.stat('/opt/mongodb').mode & 0070) != 0 }
+
+end
+
+# execute 'systemctl daemon-reload'
+execute 'daemonReload' do
+	command 'systemctl daemon-reload'
+end
+
+# subscribes properties for state changes if needed
+
+service 'mongod' do
+	action [:start, :enable]
+	# subscribes :restart, 'template[/etc/mongod.conf]', :immediate
+end
+
+
+
+
+
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,14 +5,61 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
+# unless os.windows?
   # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
-end
+#   describe user('root'), :skip do
+#     it { should exist }
+#   end
+# end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+# describe port(80), :skip do
+#   it { should_not be_listening }
+# end
+
+# our end goal - is mongo daemon running?
+
+describe command('curl http://localhost:27017') do
+  	its('stdout') { should match /MongoDB/ }
 end
+
+# users and groups
+
+describe group('mongo') do
+	it { should exist }
+end
+
+describe user('mongo') do
+	it { should exist }
+	its('groups') { should include 'mongo' }
+	its('home') { should eq '/opt/mongodb' }
+end
+
+
+
+describe package('mongodb-org') do
+	it { should be_installed }
+end
+
+
+describe file('/etc/yum.repos.d/mongodb-org-3.4.repo') do
+	it { should exist }
+end
+
+
+# directories
+
+describe directory('/opt/mongodb') do
+	it { should exist }
+	its('group') { should eq 'mongo' }
+	it { should be_executable.by('group') }
+	it { should be_readable.by('group') }
+	it { should be_writable.by('group') }
+end
+
+
+# sshd config
+ 
+# describe sshd_config do
+# 	its('PasswordAuthentication') { should eq 'yes' }
+# end
